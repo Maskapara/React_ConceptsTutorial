74b6d13540cd31a773227f780ad1888d6defd048
MasuCatz <bloodteam.a.ranger@gmail.com>
2020-08-05 01:05:44 +0900

    コンポーネントの抽出1
    https://ja.reactjs.org/docs/components-and-props.html#extracting-components

--- src/index.js
+++ src/index.js
@@ -3,22 +3,46 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-function Welcome(props){
-  return <h1>Hello,{props.name}</h1>;
+function formatDate(date) {
+  return date.toLocaleDateString();
 }
 
-function App(){
-  return(
-    <div>
-      <Welcome name="Sara" />
-      <Welcome name="Cahal" />
-      <Welcome name="Edite" />
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <div className="UserInfo">
+        <img
+          className="Avatar"
+          src={props.author.avatarUrl}
+          alt={props.author.name}
+        />
+        <div className="UserInfo-name">
+          {props.author.name}
+        </div>
+      </div>
+      <div className="Comment-text">{props.text}</div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
     </div>
-  )
+  );
 }
+
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'https://placekitten.com/g/64/64',
+  },
+};
 
 ReactDOM.render(
-  <App />,
+  <Comment
+    date={comment.date}
+    text={comment.text}
+    author={comment.author}
+  />,
   document.getElementById('root')
 );
 

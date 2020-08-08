af7c7a125893e55eb282bbf1f559a4a09b46ca9e
MasuCatz <bloodteam.a.ranger@gmail.com>
2020-08-08 23:15:17 +0900

    クラスにローカルな state を追加する
    https://ja.reactjs.org/docs/state-and-lifecycle.html#adding-local-state-to-a-class

--- src/index.js
+++ src/index.js
@@ -4,24 +4,25 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 class Clock extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {date: new Date()};
+  }
+
   render(){
     return(
     <div>
       <h1>Hello, world!</h1>
-      <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+      <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
     </div>
     );
   }
 }
 
-function tick() {
-  ReactDOM.render(
-    <Clock date={new Date()} />,
-    document.getElementById('root')
-  );
-}
-
-setInterval(tick, 1000);
+ReactDOM.render(
+  <Clock />,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
